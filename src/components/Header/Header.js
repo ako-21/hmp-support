7b09c0cac687ba47e89569336e527ed6907013a8
Akeem Ogle <aogle@hcmp.onmicrosoft.com>
2021-09-03 15:33:40 -0400

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,43 +1,33 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import { Link, NavLink } from 'react-router-dom'
-
-const authenticatedOptions = (
-  <Fragment>
-    <NavLink to='/change-password' className='nav-link'>Change Password</NavLink>
-    <NavLink to='/sign-out' className='nav-link'>Sign Out</NavLink>
-  </Fragment>
-)
-
-const unauthenticatedOptions = (
-  <Fragment>
-    <NavLink to='/sign-up' className='nav-link'>Sign Up</NavLink>
-    <NavLink to='/sign-in' className='nav-link'>Sign In</NavLink>
-  </Fragment>
-)
-
-const alwaysOptions = (
-  <Fragment>
-    <NavLink exact to='/' className='nav-link'>Home</NavLink>
-  </Fragment>
-)
+import Logo from './../../images/Logo.png'
+import Container from 'react-bootstrap/Container'
+import { AiOutlineMail } from 'react-icons/ai'
+import { IconContext } from 'react-icons'
 
 const Header = ({ user }) => (
-  <Navbar bg='primary' variant='dark' expand='md'>
-    <Navbar.Brand>
-      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>react-auth-template</Link>
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls='basic-navbar-nav' />
-    <Navbar.Collapse id='basic-navbar-nav'>
-      <Nav className='ml-auto'>
-        {user && (
-          <span className='navbar-text mr-2'>Welcome, {user.email}</span>
-        )}
-        {alwaysOptions}
-        {user ? authenticatedOptions : unauthenticatedOptions}
-      </Nav>
-    </Navbar.Collapse>
+  <Navbar style={{ borderBottom: '1px solid grey' }} bg='ghost' variant='light' expand='md'>
+    <Container className="d-flex align-items-end" style={{ paddingBottom: '.5rem', paddingTop: '.5rem' }}>
+      <Navbar.Brand>
+        <img src={Logo} style={{ width: '300px', height: '72.5px' }} alt="logo" ></img>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav style={{ width: '100%' }} className='d-flex justify-content-end'>
+          <div className="col-8 d-flex justify-content-around">
+            <Nav.Link href='+17272221245'><span className="threecx nav-font">727-222-1245</span>
+            </Nav.Link>
+            <Nav.Link href='mailto: support@hmpmetrics.com' className="nav-font">
+              <IconContext.Provider className="mr-1" value={{ color: '#304771', size: '1.5rem' }}>
+                <AiOutlineMail></AiOutlineMail>
+              </IconContext.Provider>
+              <span className="ml-5">support@hmpmetrics.com</span>
+            </Nav.Link>
+          </div>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
   </Navbar>
 )
 
